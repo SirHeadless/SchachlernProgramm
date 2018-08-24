755b748908c7aeb758f8d10f317152aea1d48ab4
creuter <christoph.reuter@seznam.cz>
2018-08-24 16:31:38 +0200

    #1 Try state pattern

--- src/store/utilities/GameUtilities.ts
+++ src/store/utilities/GameUtilities.ts
@@ -1,53 +1,122 @@
 import { Coordinate } from '../classes/gameField/Coordinate';
 import { Game } from '../classes/Game';
 import { FigureMovesUtilities } from './FigureMovesUtilities';
-import { Simulate } from 'react-dom/test-utils';
-import select = Simulate.select;
-import { Field } from '../classes/gameField/field/Field';
+
+interface BoardMarkerState {
+    markedField: Coordinate;
+    enterState: (game: Game, selectedFieldCoord: Coordinate) => void;
+    emptyFieldSelected: (game: Game, selectedFieldCoord: Coordinate) => Game;
+    figureSelected: (game: Game, selectedFieldCoord: Coordinate) => Game;
+}
 
 export class GameUtilities {
 
     static BOARD_SIZE: number = 8;
 
-    static handleFieldClick(game: Game, newSelectedFieldCoord: Coordinate): Game {
-        var selectedField = this.getSelectedField(game);
-        var isFigureSelected = selectedField != null  && selectedField.figure != null;
-        var newSelectedField = game.gameField.getField(newSelectedFieldCoord);
-        if (!isFigureSelected && newSelectedField.figure == null ) {
-            return game;
-        } else if (!isFigureSelected && newSelectedField.figure != null) {
-            return GameUtilities.selectField(game, newSelectedFieldCoord);
-        } else if (isFigureSelected &&  newSelectedField.active) {
-            return GameUtilities.moveFigure(game, game.gameField.selectedField, newSelectedFieldCoord);
-        } else if (isFigureSelected && newSelectedField.figure == null) {
-            return GameUtilities.selectField(game, newSelectedFieldCoord);
-        } else if (isFigureSelected && newSelectedField.figure != null) {
-            return GameUtilities.selectField(game, newSelectedFieldCoord);
+    static boardMarkerState: BoardMarkerState;
+
+    static NothingMarked = class {
+
+        constructor(markedField: Coordinate) {
+            this.markedField = markedField;
         }
-        return game;
+
+        markedField: Coordinate;
+
+        enterState(game: Game, selectedFieldCoord: Coordinate) {
+
+        }
+
+        emptyFieldSelected(game: Game, selectedFieldCoord: Coordinate): Game {
+            return game;
+        }
+
+        figureSelected(game: Game, selectedFieldCoord: Coordinate): Game  {
+            GameUtilities.boardMarkerState = new GameUtilities.FigureMarked(selectedFieldCoord);
+            return GameUtilities.markField(game, selectedFieldCoord);
+        }
+
 
     }
 
-    static deactivateAllFields(game: Game) {
+    static FigureMarked = class {
+
+        constructor(markedField: Coordinate) {
+            this.markedField = markedField;
+        }
+
+        markedField: Coordinate;
+
+        enterState(game: Game, selectedFieldCoord: Coordinate) {
+
+        }
+
+        emptyFieldSelected(game: Game, selectedFieldCoord: Coordinate): Game  {
+            var selectedField = game.gameField.getField(selectedFieldCoord);
+            GameUtilities.boardMarkerState = new GameUtilities.NothingMarked(selectedFieldCoord);
+            if (selectedField.active) {
+                return GameUtilities.moveFigure(game, this.markedField, selectedFieldCoord);
+            }
+            return GameUtilities.markField(game, selectedFieldCoord);
+
+        }
+
+        figureSelected(game: Game, selectedFieldCoord: Coordinate) : Game {
+            var selectedField = game.gameField.getField(selectedFieldCoord);
+            if (selectedField.active) {
+                GameUtilities.boardMarkerState = new GameUtilities.NothingMarked(null);
+                return GameUtilities.moveFigure(game, this.markedField, selectedFieldCoord);
+            }
+            GameUtilities.boardMarkerState = new GameUtilities.FigureMarked(selectedFieldCoord);
+            return GameUtilities.markField(game, selectedFieldCoord);
+
+        }
+
+    }
+
+
+    static handleFieldClick(game: Game, newSelectedFieldCoord: Coordinate): Game {
+
+        if (this.boardMarkerState == undefined) {
+            this.boardMarkerState = new GameUtilities.NothingMarked(null);
+        }
+
+        var selectedField = game.gameField.getField(newSelectedFieldCoord);
+        var isFigureSelected;
+        if (selectedField == null) {
+            isFigureSelected = false;
+        } else {
+            isFigureSelected = selectedField.figure != null;
+        }
+
+
+        if (!isFigureSelected) {
+            return this.boardMarkerState.emptyFieldSelected(game, newSelectedFieldCoord);
+        }
+        return this.boardMarkerState.figureSelected(game, newSelectedFieldCoord);
+
+    }
+
+    private static deactivateAllFields(game: Game) {
         var newGameField = game.gameField.deactivateAll();
         var newGame = {...game, gameField: newGameField};
         return newGame;
     }
 
-    static setFieldActive(game: Game, activeFieldPosition: Coordinate): Game {
+    private static setFieldActive(game: Game, activeFieldPosition: Coordinate): Game {
         var newGameField = game.gameField.setActive(activeFieldPosition);
         var newGame = {...game, gameField: newGameField};
         return newGame;
     }
 
-    static selectField(game: Game, selectedField: Coordinate): Game {
+    private static markField(game: Game, selectedField: Coordinate): Game {
         var deactivatedGameField = game.gameField.deactivateAll();
         var newGameField = FigureMovesUtilities.activateMoveFields(deactivatedGameField, selectedField).setSelectedField(selectedField);
         var newGame = {...game, gameField: newGameField};
         return newGame;
     }
 
-    static moveFigure(game: Game, fromFieldCoord: Coordinate, toFieldCoord: Coordinate): Game {
+    private static moveFigure(game: Game, fromFieldCoord: Coordinate, toFieldCoord: Coordinate): Game {
         if (toFieldCoord.equals(fromFieldCoord)) {
             return {...game};
         }
@@ -55,8 +124,4 @@ export class GameUtilities {
         return {...game, gameField: newGameField};
     }
 
-    private static getSelectedField(game: Game): Field {
-        return  (game.gameField.selectedField != null) ? game.gameField.getField(game.gameField.selectedField) : null;
-
-    }
 }
